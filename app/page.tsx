fa67419b308615b4ae940125fb6e01f0739c91a6
marciodavid <marciodavid81@gmail.com>
2024-12-10 17:48:18 -0300

    feat: ensure bookings listed are booking page

--- app/page.tsx
+++ app/page.tsx
@@ -7,33 +7,66 @@ import { quickSearchOptions } from "./_constants/search"
 import BookingItem from "./_components/booking-item"
 import Search from "./_components/search"
 import Link from "next/link"
+import { getServerSession } from "next-auth"
+import { authOptions } from "./_lib/auth"
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 const Home = async () => {
+  const session = await getServerSession(authOptions)
   const barbershops = await db.barbershop.findMany({})
   const popularBarbershops = await db.barbershop.findMany({
     orderBy: {
       name: "desc",
     },
   })
-
-  const day = new Date().toLocaleDateString("pt-BR", {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  })
+  const confirmedBookings = session?.user
+    ? await db.booking.findMany({
+        where: {
+          userId: (session.user as any).id,
+          date: {
+            gte: new Date(),
+          },
+        },
+        include: {
+          service: {
+            include: {
+              barbershop: true,
+            },
+          },
+        },
+        orderBy: {
+          date: "asc",
+        },
+      })
+    : []
 
   return (
     <div>
       <Header />
+
+      {/* BOAS VINDAS */}
       <div className="p-5">
-        <h2 className="text-xl font-bold">Olá, Marcio!</h2>
-        <p className="capitalize">{day}</p>
+        <h2 className="text-xl font-bold">
+          Olá,{" "}
+          {session?.user ? session.user.name?.split(" ")[0] : "seja bem vindo"}!
+        </h2>
+        <p>
+          <span className="capitalize">
+            {format(new Date(), "EEEE, dd", { locale: ptBR })}
+          </span>
+          <span>&nbsp;de&nbsp;</span>
+          <span className="capitalize">
+            {format(new Date(), "MMMM", { locale: ptBR })}.
+          </span>
+        </p>
 
         {/* BUSCA */}
         <div className="mt-6">
           <Search />
         </div>
 
+        {/* BOTÕES DE BUSCA RÁPIDA */}
         <div className="mt-6 flex gap-3 overflow-auto sm:justify-evenly [&::-webkit-scrollbar]:hidden">
           {quickSearchOptions.map((option) => (
             <Button
@@ -55,7 +88,8 @@ const Home = async () => {
           ))}
         </div>
 
-        <div className="relative mt-6 h-[150px] w-full">
+        {/* BANNER */}
+        <div className="relative mt-6 h-[150px] w-full md:h-0">
           <Image
             src="/banner01.png"
             fill
@@ -63,10 +97,18 @@ const Home = async () => {
             alt="Agende nos melhores com FSW Barber"
           />
         </div>
-        {/* Agendamentos   */}
 
-        <BookingItem />
+        {/* AGENDAMENTOS */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Seus agendamentos
+        </h2>
+        <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {confirmedBookings.map((booking) => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+        </div>
 
+        {/* RECOMENDADOS */}
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
         </h2>
@@ -76,6 +118,8 @@ const Home = async () => {
             <BarberShopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
+
+        {/* POPULARES */}
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Populares
         </h2>
